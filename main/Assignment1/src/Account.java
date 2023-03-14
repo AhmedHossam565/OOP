dee79cbed156c92fa745bdcf5cb464fa2c6c4e55
AhmedHossam565 <ahossam565@gmail.com>
2023-03-14 19:34:12 +0200

    the first file

---
+++ main/Assignment1/src/Account.java
@@ -0,0 +1,66 @@
+import java.util.Date;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+
+    public Account() {
+        id = 0;
+        balance = 0;
+        annualInterestRate = 0;
+        dateCreated = new Date();
+    }
+
+    public Account(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
+        annualInterestRate = 0;
+        dateCreated = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double rate) {
+        annualInterestRate = rate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public double getMonthlyInterestRate() {
+        return annualInterestRate / 12.0;
+    }
+
+    public double getMonthlyInterest() {
+        return balance * getMonthlyInterestRate();
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+}
